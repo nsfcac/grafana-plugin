bda005b338b1b29a2df4d9b83dc02b401bd96239
Zipexpo <nvtngan@hcmus.edu.vn>
2022-04-04 16:50:59 -0500

    update plugin

--- parallel-coordinate/src/ControlPannel/LegendEditor.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ColorPicker } from '@grafana/ui';
-import { StandardEditorProps } from '@grafana/data';
-
-
-export const LegendEditor: React.FC<StandardEditorProps<string[]>> = ({ value,item, onChange }) => {
-    return <>{value.map((c,i)=><div style={{display:'inline-block'}}><ColorPicker key={i} color={c} onChange={(val) => {value[i]=val; onChange(value)}} /></div>)}</>;
-};
